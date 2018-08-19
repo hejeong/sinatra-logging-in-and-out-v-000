320d4fde9f85435e9b0a444950afde20791d8568
Jonathan Hong <jonhong777@gmail.com>
2018-08-19 19:11:20 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,9 @@
 class Helpers
   def self.current_user(session)
-    User.find_by(id: session[:user_id])
+    User.find_by_id(id: session[:user_id])
   end
 
   def self.is_logged_in?
-    
+
   end
 end
